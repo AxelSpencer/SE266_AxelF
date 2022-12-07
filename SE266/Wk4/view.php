bcf5386809713e5e4b08dd12ed77808151ca1269
AxelSpencer <97459157+AxelSpencer@users.noreply.github.com>
2022-12-07 03:18:32 -0500

    Update view.php

--- SE266/Wk4/view.php
+++ SE266/Wk4/view.php
@@ -7,8 +7,6 @@
     <title>Patients | Main </title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
     <style type="text/css">
         body 
         {
